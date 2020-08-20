6e27db68942cf37e3cf0333bac84dfb530a7b38f
Sung Paik <sung.paik@codeimmersives.com>
2020-08-20 11:55:01 -0400

    switched conditional

--- client/src/lib/Axios/Axios.js
+++ client/src/lib/Axios/Axios.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const Axios = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
-      ? "http://localhost:3001/api"
-      : "/api",
+      ? "/api"
+      : "http://localhost:3001/api",
   timeout: 50000,
   headers: {
     Accept: "application/json",
